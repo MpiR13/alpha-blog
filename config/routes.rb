c01f2848b5a822b2fae189c992276431163e1a38
MichaelRay <mcray.mos.2015@gmail.com>
2017-09-09 22:07:29 -0400

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,13 @@ get 'signup' => 'users#new'
 resources :users, except: [:new]
 # post 'users' => 'users#create' instead of what's above this
 
+get 'login' => 'sessions#new'
+post 'login' => 'sessions#create'
+delete 'logout' => 'sessions#destroy'
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes" or "rails routes".
+
+  # You can have the root of your site routed with "root"
+
 end
